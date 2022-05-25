bf65072d267936bb0f7594fc7d8ab488ab14a625
guptashivam9 <shivam98042@gmail.com>
2022-05-25 13:02:29 +0545

    first commit

---
+++ 1_Introduction/chapter4_object_class/iteminfo.cpp
@@ -0,0 +1,15 @@
+#include<iostrem>
+using namespace std;
+class iteminfo
+{
+    private:
+    int itemid;
+     float cost;
+     public:
+     void set data(int it,float cst)
+     {
+         itemid=it;
+         cost=cst;
+     }
+     void showdata
+
