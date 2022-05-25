9f19d3ca14d709de77a921a298e178db76a2d390
guptashivam9 <105804900+guptashivam9@users.noreply.github.com>
2022-05-25 14:08:52 +0545

    initial commit

---
+++ 1_Introduction/1_Aot.cpp
@@ -0,0 +1,19 @@
+//q.Wap to calculate the area of triangle.//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int height;
+    int base;
+    int area;
+    cout<<"Enter the height: ";
+    cin>>height;
+
+    cout<<"Enter the breadth: ";
+    cin>>base;
+
+    area = 0.5*height*base;
+    cout<<"Area of triangle is: " <<area;
+
+}
